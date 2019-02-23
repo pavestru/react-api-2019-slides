832fd9cd5009c77717de215b36b65d874a3c801c
Pavel Struhar <pstruhar@gmail.com>
2019-02-23 20:19:12 +0100

    Syntax highlighting

--- theme.js
+++ theme.js
@@ -1,8 +1,11 @@
 import theme from 'mdx-deck/themes'
+import okaidia from 'react-syntax-highlighter/styles/prism/prism'
 
 export default {
   ...theme,
-
+  prism: {
+    style: okaidia
+  }
   // Customize your presentation theme here.
   //
   // Read the docs for more info:
